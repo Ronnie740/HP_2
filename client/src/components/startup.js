409c91b6990163fecbb3fe34d2c62b9fa1f38dbe
ronnie <ronnieineza@gmail.com>
2023-06-19 19:05:51 +0300

    added the startup registration page and created a the template for the startups.

--- client/src/components/startup.js
+++ client/src/components/startup.js
@@ -17,6 +17,88 @@ const ProgressBar = ({ value, maxValue }) => {
 		</div>
 	);
 };
+const StartupTemplate = ({ startupName, startupDesc, startupCountry, startupCategory, goalPercentage, goal, problemSolved, dangers, solution, videos, teamMembers }) => {
+	return (
+		<main className='mx-20'>
+			{/* Startup Bio */}
+			<section className='bg-slate-200 py-10 my-10 rounded-md shadow-lg'>
+				<div className='grid grid-cols-2 gap-5 m-5'>
+					<div className='flex flex-col space-y-5 text-center'>
+						<h1 className='text-2xl font-semibold'>{startupName}</h1>
+						<img src={money} alt='img' className='w-auto h-auto max-h-fit rounded-md shadow-lg' />
+						<p className='text-xl'>{startupDesc}</p>
+						<div className='flex justify-center space-x-5 text-2xl'>
+							<p>{startupCountry}</p>
+							<p>{startupCategory}</p>
+						</div>
+					</div>
+					<div className='flex flex-col my-auto text-center space-y-4'>
+						<div className='flex justify-center'>
+							<ProgressBar maxValue={100} value={goalPercentage} />
+						</div>
+						{/* Percentage of donations to goal */}
+						<p className='text-xl font-semibold'>{`${goalPercentage} % raised`}</p>
+						<h2 className='text-2xl'>Goal</h2>
+						<p className='text-xl font-semibold'>{goal}</p>
+						<div className='w-full justify-center'>
+							<button className='w-fit bg-primary hover:bg-button_active px-5 py-3 text-white rounded-md font-semibold'>Add to favourites</button>
+						</div>
+					</div>
+				</div>
+			</section>
+			{/* Problems being solved */}
+			<section className='bg-slate-200 py-10 my-10 rounded-md shadow-lg text-center flex flex-col justify-center'>
+				<div className='flex flex-col'>
+					<h1 className='text-2xl font-semibold'> Problems being solved</h1>
+					<p>{problemSolved}</p>
+				</div>
+				{/* Danger Posed */}
+				<div className='flex flex-col m-5'>
+					<h1 className='text-2xl font-semibold'>Dangers Posed</h1>
+					<div className='grid grid-cols-2 gap-5 mx-auto w-1/2 my-5'>
+						{dangers.map((danger, index) => (
+							<div className='flex mx-auto' key={index}>
+								<Tabs title={danger.title} imageSrc={danger.image} />
+							</div>
+						))}
+					</div>
+				</div>
+				<div className='flex flex-col'>
+					<h1 className='text-2xl font-semibold'> Solution</h1>
+					<p>{solution}</p>
+				</div>
+				{/* Videos */}
+				<div className='flex flex-col m-5'>
+					<h1 className='text-2xl font-semibold'>Videos</h1>
+					<div className='grid grid-cols-2 gap-5 mx-auto w-full my-5'>
+						{videos.map((video, index) => (
+							<div className='flex mx-auto h-80' key={index}>
+								<ReactPlayer url={video.url} width='560px' height='315px' controls />
+							</div>
+						))}
+					</div>
+				</div>
+			</section>
+
+			{/* Team members */}
+			<section className='bg-slate-200 py-10 my-10 rounded-md shadow-lg text-center flex flex-col justify-center w-auto'>
+				<div className='flex flex-col m-5'>
+					<h1 className='text-2xl font-semibold'>Meet The Team</h1>
+					<div className='grid grid-cols-2 gap-5 mx-auto w-1/2 my-5'>
+						{teamMembers.map((member, index) => (
+							<div className='flex mx-auto' key={index}>
+								<Tabs title={member.name} imageSrc={member.image} subTitle={member.title} description={member.description} />
+							</div>
+						))}
+					</div>
+				</div>
+			</section>
+			<div className='my-5'>
+				<Button label='Donate' onClick='' />
+			</div>
+		</main>
+	);
+};
 function Startup() {
 	return (
 		// <main className='mx-20'>
--- client/src/components/startup_reg.js
+++ client/src/components/startup_reg.js
@@ -1,53 +1,426 @@
 /** @format */
 
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 // axios.defaults.baseURL = 'http://localhost:4000';
 
+// const StartupReg = () => {
+// 	const handleSubmit = async (event) => {
+// 		event.preventDefault();
+
+// 		try {
+// 			const newStartup = {
+// 				name: 'Example Startup',
+// 				description: 'This is an example startup',
+// 				problems: 'Some problems',
+// 				dangers: 'Potential dangers',
+// 				solution: 'Proposed solution',
+// 				video_urls: 'http://example.com/videos',
+// 				team: [
+// 					{
+// 						name: 'John Doe',
+// 						jobTitle: 'CEO',
+// 						description: 'Some description',
+// 					},
+// 					{
+// 						name: 'Jane Smith',
+// 						jobTitle: 'CTO',
+// 						description: 'Another description',
+// 					},
+// 				],
+// 			};
+
+// 			const response = await axios.post('/startups', newStartup);
+// 			console.log(response.data); // Handle the response as needed
+// 		} catch (error) {
+// 			console.error(error);
+// 		}
+// 	};
+
+// 	return (
+// 		<div className='flex'>
+// 			<form onSubmit={handleSubmit} className='mx-auto'>
+// 				{/* form fields */}
+// 				<button type='submit' className='p-5 rounded-md hover:bg-button_active m-5 bg-primary text-white text-3xl font-bold capitalize'>
+// 					Submit
+// 				</button>
+// 			</form>
+// 		</div>
+// 	);
+// };
 const StartupReg = () => {
-	const handleSubmit = async (event) => {
-		event.preventDefault();
+	const [startupData, setStartupData] = useState({
+		startupName: '',
+		startupDesc: '',
+		startupCountry: '',
+		startupCategory: '',
+		// goalPercentage: '',
+		paypalLink: '',
+		goal: '',
+		problemSolved: '',
+		dangers: [],
+		solution: '',
+		videos: [],
+		teamMembers: [],
+	});
 
-		try {
-			const newStartup = {
-				name: 'Example Startup',
-				description: 'This is an example startup',
-				problems: 'Some problems',
-				dangers: 'Potential dangers',
-				solution: 'Proposed solution',
-				video_urls: 'http://example.com/videos',
-				team: [
-					{
-						name: 'John Doe',
-						jobTitle: 'CEO',
-						description: 'Some description',
-					},
-					{
-						name: 'Jane Smith',
-						jobTitle: 'CTO',
-						description: 'Another description',
-					},
-				],
+	const handleInputChange = (e) => {
+		const { name, value } = e.target;
+		setStartupData((prevState) => ({
+			...prevState,
+			[name]: value,
+		}));
+	};
+
+	const handleAddDanger = () => {
+		setStartupData((prevState) => ({
+			...prevState,
+			dangers: [...prevState.dangers, ''],
+		}));
+	};
+
+	const handleRemoveDanger = (index) => {
+		setStartupData((prevState) => {
+			const updatedDangers = [...prevState.dangers];
+			updatedDangers.splice(index, 1);
+			return {
+				...prevState,
+				dangers: updatedDangers,
 			};
+		});
+	};
 
-			const response = await axios.post('/startups', newStartup);
-			console.log(response.data); // Handle the response as needed
-		} catch (error) {
-			console.error(error);
-		}
+	const handleDangerInputChange = (e, index) => {
+		const { value } = e.target;
+		setStartupData((prevState) => {
+			const updatedDangers = [...prevState.dangers];
+			updatedDangers[index] = value;
+			return {
+				...prevState,
+				dangers: updatedDangers,
+			};
+		});
+	};
+
+	const handleAddVideo = () => {
+		setStartupData((prevState) => ({
+			...prevState,
+			videos: [...prevState.videos, ''],
+		}));
+	};
+
+	const handleRemoveVideo = (index) => {
+		setStartupData((prevState) => {
+			const updatedVideos = [...prevState.videos];
+			updatedVideos.splice(index, 1);
+			return {
+				...prevState,
+				videos: updatedVideos,
+			};
+		});
+	};
+
+	const handleVideoInputChange = (e, index) => {
+		const { value } = e.target;
+		setStartupData((prevState) => {
+			const updatedVideos = [...prevState.videos];
+			updatedVideos[index] = value;
+			return {
+				...prevState,
+				videos: updatedVideos,
+			};
+		});
+	};
+
+	const handleAddTeamMember = () => {
+		setStartupData((prevState) => ({
+			...prevState,
+			teamMembers: [...prevState.teamMembers, { name: '', title: '', description: '' }],
+		}));
+	};
+
+	const handleRemoveTeamMember = (index) => {
+		setStartupData((prevState) => {
+			const updatedTeamMembers = [...prevState.teamMembers];
+			updatedTeamMembers.splice(index, 1);
+			return {
+				...prevState,
+				teamMembers: updatedTeamMembers,
+			};
+		});
+	};
+
+	const handleTeamMemberInputChange = (e, index, field) => {
+		const { value } = e.target;
+		setStartupData((prevState) => {
+			const updatedTeamMembers = [...prevState.teamMembers];
+			updatedTeamMembers[index][field] = value;
+			return {
+				...prevState,
+				teamMembers: updatedTeamMembers,
+			};
+		});
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		// Send startupData to the server
+		console.log(startupData);
+		// Reset the form fields
+		setStartupData({
+			startupName: '',
+			startupDesc: '',
+			startupCountry: '',
+			startupCategory: '',
+			// goalPercentage: '',
+			paypalLink: '',
+			goal: '',
+			problemSolved: '',
+			dangers: [],
+			solution: '',
+			videos: [],
+			teamMembers: [],
+		});
 	};
 
 	return (
-		<div className='flex'>
-			<form onSubmit={handleSubmit} className='mx-auto'>
-				{/* form fields */}
-				<button type='submit' className='p-5 rounded-md hover:bg-button_active m-5 bg-primary text-white text-3xl font-bold capitalize'>
-					Submit
+		<main className='mx-20 bg-slate-200 m-10 p-5 rounded-md'>
+			<form className='' onSubmit={handleSubmit}>
+				{/* Startup Name */}
+				<div className='flex flex-row space-x-10'>
+					<div className='w-full'>
+						<label htmlFor='startupName' className=' text-2xl font-semibold'>
+							Startup Name
+						</label>
+						<input
+							type='text'
+							id='startupName'
+							name='startupName'
+							value={startupData.startupName}
+							placeholder='The name of the startup'
+							onChange={handleInputChange}
+							className='w-full bg-white border border-gray-300 py-2 px-4 rounded-md shadow-sm mt-2'
+							required
+						/>
+					</div>
+
+					{/* Startup Country */}
+					<div className='w-full'>
+						<label htmlFor='startupCountry' className=' text-2xl font-semibold mt-6'>
+							Startup Country
+						</label>
+						<input
+							type='text'
+							id='startupCountry'
+							name='startupCountry'
+							value={startupData.startupCountry}
+							placeholder='Country where the startup is based'
+							onChange={handleInputChange}
+							className='w-full bg-white border border-gray-300 py-2 px-4 rounded-md shadow-sm mt-2'
+							required
+						/>
+					</div>
+				</div>
+				{/* Startup Description */}
+				<label htmlFor='startupDesc' className='block text-2xl font-semibold mt-6'>
+					Startup Description
+				</label>
+				<textarea
+					id='startupDesc'
+					name='startupDesc'
+					value={startupData.startupDesc}
+					onChange={handleInputChange}
+					className='w-full bg-white border border-gray-300 py-2 px-4 rounded-md shadow-sm mt-2'
+					required
+				/>
+
+				{/* Startup Category */}
+				<label htmlFor='startupCategory' className='block text-2xl font-semibold mt-6'>
+					Startup Category
+				</label>
+				<input
+					type='text'
+					id='startupCategory'
+					name='startupCategory'
+					value={startupData.startupCategory}
+					placeholder='What category does this startups fall under?'
+					onChange={handleInputChange}
+					className='w-full bg-white border border-gray-300 py-2 px-4 rounded-md shadow-sm mt-2'
+					required
+				/>
+
+				{/* Goal Percentage */}
+				<div className='flex flex-row space-x-10'>
+					{/* <div className='w-full'>
+						<label htmlFor='goalPercentage' className='block text-2xl font-semibold mt-6'>
+							Goal Percentage
+						</label>
+						<input
+							type='number'
+							id='goalPercentage'
+							name='goalPercentage'
+							value={startupData.goalPercentage}
+							onChange={handleInputChange}
+							className='w-full bg-white border border-gray-300 py-2 px-4 rounded-md shadow-sm mt-2'
+							required
+						/>
+					</div> */}
+					<div className='w-full'>
+						<label htmlFor='paypalLink' className='block text-2xl font-semibold mt-6'>
+							Paypal.Me Link
+						</label>
+						<input
+							type='text'
+							id='paypalLink'
+							name='paypalLink'
+							value={startupData.paypalLink}
+							placeholder='Please enter your Paypal.me link to facilitate the donations'
+							onChange={handleInputChange}
+							className='w-full bg-white border border-gray-300 py-2 px-4 rounded-md shadow-sm mt-2'
+							required
+						/>
+					</div>
+
+					{/* Goal */}
+					<div className='w-full'>
+						<label htmlFor='goal' className='block text-2xl font-semibold mt-6'>
+							Goal Amount
+						</label>
+						<input
+							type='text'
+							id='goal'
+							name='goal'
+							placeholder='Goal amount needed'
+							value={startupData.goal}
+							onChange={handleInputChange}
+							className='w-full bg-white border border-gray-300 py-2 px-4 rounded-md shadow-sm mt-2'
+							required
+						/>
+					</div>
+				</div>
+
+				{/* Problem Solved */}
+				<label htmlFor='problemSolved' className='block text-2xl font-semibold mt-6'>
+					Problem this startup is solving
+				</label>
+				<textarea
+					id='problemSolved'
+					name='problemSolved'
+					value={startupData.problemSolved}
+					placeholder='Problems being solved'
+					onChange={handleInputChange}
+					className='w-full bg-white border border-gray-300 py-2 px-4 rounded-md shadow-sm mt-2'
+					required
+				/>
+
+				{/* Dangers */}
+				<label className='block text-2xl font-semibold mt-6'>Dangers posed by the problems your solving</label>
+				{startupData.dangers.map((danger, index) => (
+					<div key={index} className='flex mt-2'>
+						<input
+							type='text'
+							name='danger'
+							value={danger}
+							placeholder='Danger'
+							onChange={(e) => handleDangerInputChange(e, index)}
+							className='w-full bg-white border border-gray-300 py-2 px-4 rounded-md shadow-sm'
+							required
+						/>
+						<button type='button' onClick={() => handleRemoveDanger(index)} className='ml-2 bg-red-500 hover:bg-red-600 text-white px-3 py-2 rounded-md'>
+							Remove
+						</button>
+					</div>
+				))}
+				<button type='button' onClick={handleAddDanger} className='bg-green-500 hover:bg-green-600 text-white px-3 py-2 rounded-md mt-2'>
+					Add Danger
 				</button>
+
+				{/* Solution */}
+				<label htmlFor='solution' className='block text-2xl font-semibold mt-6'>
+					Solution
+				</label>
+				<textarea
+					id='solution'
+					name='solution'
+					value={startupData.solution}
+					placeholder='Your solution'
+					onChange={handleInputChange}
+					className='w-full bg-white border border-gray-300 py-2 px-4 rounded-md shadow-sm mt-2'
+					required
+				/>
+
+				{/* Videos */}
+				<label className='block text-2xl font-semibold mt-6'>Video Link</label>
+				{startupData.videos.map((video, index) => (
+					<div key={index} className='flex mt-2'>
+						<input
+							type='text'
+							name='video'
+							value={video}
+							onChange={(e) => handleVideoInputChange(e, index)}
+							className='w-full bg-white border border-gray-300 py-2 px-4 rounded-md shadow-sm'
+							required
+						/>
+						<button type='button' onClick={() => handleRemoveVideo(index)} className='ml-2 bg-red-500 hover:bg-red-600 text-white px-3 py-2 rounded-md'>
+							Remove
+						</button>
+					</div>
+				))}
+				<button type='button' onClick={handleAddVideo} className='bg-green-500 hover:bg-green-600 text-white px-3 py-2 rounded-md mt-2'>
+					Add Video
+				</button>
+
+				{/* Team Members */}
+				<label className='block text-2xl font-semibold mt-6'>Team Members</label>
+				{startupData.teamMembers.map((teamMember, index) => (
+					<div key={index} className='flex flex-col mt-4'>
+						<input
+							type='text'
+							name='name'
+							value={teamMember.name}
+							onChange={(e) => handleTeamMemberInputChange(e, index, 'name')}
+							className='w-full bg-white border border-gray-300 py-2 px-4 rounded-md shadow-sm'
+							placeholder='Name'
+							required
+						/>
+						<input
+							type='text'
+							name='title'
+							value={teamMember.title}
+							onChange={(e) => handleTeamMemberInputChange(e, index, 'title')}
+							className='w-full bg-white border border-gray-300 py-2 px-4 rounded-md shadow-sm mt-2'
+							placeholder='Title'
+							required
+						/>
+						<textarea
+							name='description'
+							value={teamMember.description}
+							onChange={(e) => handleTeamMemberInputChange(e, index, 'description')}
+							className='w-full bg-white border border-gray-300 py-2 px-4 rounded-md shadow-sm mt-2'
+							placeholder='Description'
+							required
+						/>
+						<button type='button' onClick={() => handleRemoveTeamMember(index)} className='bg-red-500 hover:bg-red-600 text-white px-3 py-2 rounded-md mt-2'>
+							Remove
+						</button>
+					</div>
+				))}
+				<div className='flex flex-col space-y-10'>
+					<button type='button' onClick={handleAddTeamMember} className='w-fit bg-green-500 hover:bg-green-600 text-white px-3 py-2 rounded-md mt-2'>
+						Add Team Member
+					</button>
+
+					{/* Submit Button */}
+					<button type='submit' className='bg-primary hover:bg-button_active text-white px-5 py-3 rounded-md mt-6'>
+						Submit
+					</button>
+				</div>
 			</form>
-		</div>
+		</main>
 	);
 };
 
 export default StartupReg;
+
+// export default StartupReg;
