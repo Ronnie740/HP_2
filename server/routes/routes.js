a5d38e4d942f2d6f5a4d4af0f02fbde7694b8a6e
ronnie <ronnieineza@gmail.com>
2023-07-03 17:03:19 +0300

    removed comments

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -616,55 +616,6 @@ const trainRecommender = async () => {
 
 trainRecommender();
 
-// const documents = [
-// 	{ id: '1000001', content: 'Why studying javascript is fun?' },
-// 	{ id: '1000002', content: 'The trend for javascript in machine learning' },
-// 	{ id: '1000003', content: 'The most insightful stories about JavaScript' },
-// 	{ id: '1000004', content: 'Introduction to Machine Learning' },
-// 	{ id: '1000005', content: 'Machine learning and its application' },
-// 	{ id: '1000006', content: 'Python vs Javascript, which is better?' },
-// 	{ id: '1000007', content: 'How Python saved my life?' },
-// 	{ id: '1000008', content: 'The future of Bitcoin technology' },
-// 	{ id: '1000009', content: 'Is it possible to use javascript for machine learning?' },
-// ];
-
-// // start training
-// recommender.train(documents);
-
-// //get top 10 similar items to document 1000002
-// const similarDocuments = recommender.getSimilarDocuments('1000002', 0, 10);
-
-// console.log(similarDocuments);
-
-//Working
-// Route to retrieve recommended startups for a user
-// router.get('/recommendations', async (req, res) => {
-// 	try {
-// 		const { userId } = req.query;
-
-// 		// Retrieve user preferences from the database
-// 		const user = await User.findById(userId);
-// 		console.log(`User favourites :` + user.favorites);
-
-// 		const favourite = await Startup.findById(user.favorites[0]);
-
-// 		console.log(`Favourite Startup: ` + favourite._id.toString());
-// 		// Generate recommendations based on user preferences
-// 		const similarDocuments = recommender.getSimilarDocuments(favourite._id.toString(), 0, 10);
-// 		console.log(`Similar Documents`, similarDocuments);
-// 		const recommendedStartupIds = similarDocuments.map((doc) => doc.id);
-// 		console.log(`Recommended Documents`, recommendedStartupIds);
-
-// 		// Retrieve the recommended startups from the database
-// 		const recommendedStartups = await Startup.find({ _id: { $in: recommendedStartupIds } });
-
-// 		res.json(recommendedStartups);
-// 	} catch (error) {
-// 		console.error('Error retrieving recommendations:', error);
-// 		res.status(500).json({ error: 'An error occurred' });
-// 	}
-// });
-
 router.get('/recommendations', async (req, res) => {
 	try {
 		const { userId } = req.query;
